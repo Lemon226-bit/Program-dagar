71d09b2aa2361f1bfe05da13d139cb75efd44bc3
theodorejohannesson <theodore.johannesson@elev.ga.lbs.se>
2026-01-09 15:21:57 +0100

    Playerbasestats
    added some basestats for player

--- out of this world/Assets/script/PlayerStats.cs
+++ out of this world/Assets/script/PlayerStats.cs
@@ -1,8 +1,46 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerStats : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //Playermovement
+    public float baseSpeed = 3f;
+    public int speedLevel = 0;
+    public int maxSpeedLevel = 5;
+    public float speedUpgradeAmount = 3f;
+
+    public void UpgradeSpeed()
+    {
+        if (speedLevel < maxSpeedLevel)
+        {
+            speedLevel++;
+            baseSpeed += speedUpgradeAmount;
+        }
+    }
+
+    //Fuel
+    public float baseFuel = 20f;
+    public int fuelLevel = 0;
+    public int maxFuelLevel = 5;
+    public float fuelUpgradeAmount = 20f;
+
+    public void UpgradeFuel()
+    {
+        if (fuelLevel < maxFuelLevel)
+        {
+            fuelLevel++;
+            baseFuel += fuelUpgradeAmount;
+        }
+
+    }
+
+
+
+
+
+
+
+
     void Start()
     {
         

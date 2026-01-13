f6b303126b68cb229dff3d4503c4072d3a01318a
theodorejohannesson <theodore.johannesson@elev.ga.lbs.se>
2026-01-13 15:06:09 +0100

    moon!
    moon!

---
+++ out of this world/Assets/script/moon.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public class TimerToVictory : MonoBehaviour
+    {
+        float elapsedTime = 0f;
+        float victoryTime = 600f; 
+
+        void Update()
+        {
+            elapsedTime += Time.deltaTime;
+
+            if (elapsedTime >= victoryTime)
+            {
+                SceneManager.LoadScene("scene på månen");
+               
+            }
+        }
+    }
+}

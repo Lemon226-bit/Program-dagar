a6f080f370b98df80252bf57056fbb835e5a15f7
Lemon226-bit <oliver.lindskog@elev.ga.lbs.se>
2026-01-14 12:59:51 +0100

    ändring på små sak

--- out of this world/Assets/script/movement.cs
+++ out of this world/Assets/script/movement.cs
@@ -1,11 +1,10 @@
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class movment : MonoBehaviour
 {
     Rigidbody2D rb;
-    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -49,33 +48,14 @@ public class movment : MonoBehaviour
             transform.position = new Vector2(0, -2.4f);
 
         }
-        if ((transform.position.y >= 3))
-        {
-            transform.position = new Vector2(0, -2.4f);
-                
 
-        }
-        if ((transform.position.y >= -5))
-        {
-            transform.position = new Vector2(0, -2.4f);
-
-        }
-        void OnCollisionEnter2D(Collision2D collision)
-        {
-            if (collision.gameObject)
-            {
-                Destroy(gameObject);
-                SceneManager.LoadSceneAsync("gameover");
-            }
-
-        }
 
 
     }
-    
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        
+
         if (collision.gameObject)
         {
             Destroy(gameObject);

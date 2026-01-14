9618d419b03225d5a3451b4cec9e7cefca7d00b8
theodorejohannesson <theodore.johannesson@elev.ga.lbs.se>
2026-01-14 14:15:49 +0100

    cutscene kod

--- out of this world/Assets/script/moon.cs
+++ out of this world/Assets/script/moon.cs
@@ -3,27 +3,57 @@ using UnityEngine.SceneManagement;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    float elapsedTime = 0f;
+    float victoryTime = 30f;
+
+
+
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime >= victoryTime)
+        {
+            SceneManager.LoadScene("scene på månen");
+
+        }
+
+
+    }
+
+
+
     void Start()
     {
-        
+
+
     }
 
-    // Update is called once per frame
-    public class TimerToVictory : MonoBehaviour
-    {
-        float elapsedTime = 0f;
-        float victoryTime = 30f; 
 
-        void Update()
-        {
-            elapsedTime += Time.deltaTime;
 
-            if (elapsedTime >= victoryTime)
-            {
-                SceneManager.LoadScene("scene på månen");
-               
-            }
-        }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+    
+
+
+
+
+        
+        
+    
+

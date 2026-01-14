cf0840c43954791cce7261e313d2bbfc47049760
Lemon226-bit <oliver.lindskog@elev.ga.lbs.se>
2026-01-14 15:43:37 +0100

    små ändring bug fix

--- out of this world/Assets/script/DestroyEnemyOnCollision.cs
+++ out of this world/Assets/script/DestroyEnemyOnCollision.cs
@@ -5,12 +5,20 @@ public class DestroyEnemyOnCollision : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject)
+        {
+            Destroy(gameObject);
+        }
     }
 }

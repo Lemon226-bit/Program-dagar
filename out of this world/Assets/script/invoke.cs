45fd4820fecc00eea66646653eb2bc58de386ea0
theodorejohannesson <theodore.johannesson@elev.ga.lbs.se>
2026-01-15 13:46:26 +0100

    values/testing

--- out of this world/Assets/script/invoke.cs
+++ out of this world/Assets/script/invoke.cs
@@ -6,7 +6,7 @@ public class invoke : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Invoke("ChangeToNewGame", 9);
+        Invoke("ChangeToNewGame", 7);
     }
 
     // Update is called once per frame
